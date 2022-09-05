9915e95c2b389a820cde712cb72d3d84b80700c6
Dhruba Jyoti Chakraborty <61685945+codewdhruv@users.noreply.github.com>
2022-09-05 19:57:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const request = require("request");
 const express = require('express');
 
 // Constants
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || 'localhost';
 
 // App
